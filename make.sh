07d3467b0eec03bdc9d53102377ea456f2f1ea78
Your Name <you@example.com>
2026-02-17 01:36:26 -0300

    Atualizado make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -x
 
+set -e
+
+cd web/frpc
+
+npm install
+npm run build
+
+cd -
+
 export CGO_ENABLED=1
 
-exec go build -trimpath -ldflags "-s -w" -tags frpc -o bin/frpc ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -tags frpc -o bin/frpc ./cmd/frpc
+
+exit 0
+
